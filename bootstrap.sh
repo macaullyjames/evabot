68a599e3aaa7eb384665a1c136f3c4a2faff89d1
Macaully James Muir <dev@macaullyjamesmuir.com>
2016-06-16 03:22:07 +0200

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,8 +1,31 @@
-sudo apt-get update -y
-sudo apt-get install
+#!/bin/bash
 
-gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
-\curl -sSL https://get.rvm.io | bash -s stable --ruby
+logfile="/vagrant/bootstrap.log"
+rm -f $logfile
 
-source /home/vagrant/.rvm/scripts/rvm
-gem install rails -v 5.0.0.rc1
+function silent() {
+  "$@" >> $logfile 2>&1
+  if [ $? -ne 0 ]; then 
+    echo "Oops, something bad happened!" 1>&2
+    echo "See bootstrap.log for more info." 1>&2
+    exit 1
+  fi
+}
+
+echo 'Updating the box...'
+silent sudo apt-get update -y
+
+echo 'Installing rvm and ruby...'
+silent gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
+curl -sSL https://get.rvm.io | silent bash -s stable --ruby
+source ~/.rvm/scripts/rvm
+
+echo 'Installing gems...'
+cd /vagrant/app
+silent gem install bundler
+silent bundle install
+
+echo 'Sprinkling magic...'
+echo 'cd /vagrant' >> ~/.bashrc
+
+rm $logfile
