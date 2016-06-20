a3bbaab192a8cabab6c8c35e30052b8e18b3afd3
Macaully James Muir <dev@macaullyjamesmuir.com>
2016-06-20 11:15:55 +0200

    Revert "Add basic webhooks support"
    This reverts commit 3d352d6ad6182b2dc0573b0d7540edaed2b11001.

--- rails/config/routes.rb
+++ rails/config/routes.rb
@@ -1,3 +1,3 @@
 Rails.application.routes.draw do
-  resource :hooks, only: :create, defaults: { formats: :json }
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
