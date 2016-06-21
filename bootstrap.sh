225fe6a8173df73e4db0b39f7d380744231c5fe3
Macaully James Muir <dev@macaullyjamesmuir.com>
2016-06-22 00:36:13 +0200

    Add rugged gem
    This is a seperate commit since there's a good chance the CI/production
    servers will throw a tantrum over the native extensions.

--- bootstrap.sh
+++ bootstrap.sh
@@ -15,6 +15,9 @@ function silent() {
 echo 'Updating the box...'
 silent sudo apt-get update -y
 
+# Required for the "rugged" gem
+silent apt-get install cmake -y
+
 echo 'Installing rvm and ruby...'
 silent gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
 curl -sSL https://get.rvm.io | silent bash -s stable --ruby
