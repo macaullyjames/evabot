150ec28eb9a6fa1fa50049e82c91b6ccf5476408
Macaully James Muir <dev@macaullyjamesmuir.com>
2016-06-22 00:09:09 +0200

    Clean up routes.rb

--- rails/config/routes.rb
+++ rails/config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
-  get 'dashboard/index'
-
-  get 'welcome/index'
-  resources :sessions, only: [:new, :create, :destroy]
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'welcome#index'
+
+  resources :welcome,   only: [:index]
+  resources :dashboard, only: [:index]
+  resources :sessions,  only: [:new, :create, :destroy]
 end
