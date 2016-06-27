4b63b3ef6c1cb510bcfd2bdd52985339611dd4e1
Macaully James Muir <dev@macaullyjamesmuir.com>
2016-06-27 17:07:13 +0200

    Add Repo model

--- rails/db/schema.rb
+++ rails/db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160621095454) do
+ActiveRecord::Schema.define(version: 20160627143438) do
+
+  create_table "repos", force: :cascade do |t|
+    t.integer  "user_id"
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_repos_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "username"
