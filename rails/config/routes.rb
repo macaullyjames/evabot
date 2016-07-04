0b7b95df32aefa0f1f7ed2f46b15c16c4a54de74
Macaully James Muir <dev@macaullyjamesmuir.com>
2016-07-04 22:44:36 +0200

    Simplify routes

--- rails/config/routes.rb
+++ rails/config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root 'sessions#new'
 
-  resources :welcome,   only: [:index]
-  resources :dashboard, only: [:index]
-  resources :sessions,  only: [:new, :create, :destroy]
+  get 'welcome/index'
+  get 'dashboard/index'
+  resources :sessions, only: [:new, :create, :destroy]
 end
