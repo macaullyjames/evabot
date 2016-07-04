8f9a7872ea4aa5ef6b9e3587d4379f686ff26e2c
Macaully James Muir <dev@macaullyjamesmuir.com>
2016-07-04 15:54:56 +0200

    Update Vagrant up script
    Run migrations, fancier logging.

--- vagrant/up.sh
+++ vagrant/up.sh
@@ -1,5 +1,21 @@
+logfile="/evabot/up.log"
+rm -f $logfile
+
+function silent() {
+  "$@" >> $logfile 2>&1
+  if [ $? -ne 0 ]; then 
+    echo "Oops, something bad happened!" 1>&2
+    echo "See up.log for more info." 1>&2
+    exit 1
+  fi
+}
+
+cd /evabot/rails
+
 echo "Starting rails server..."
-cd /evabot/rails && rails server --daemon -b 0.0.0.0 > /dev/null
+silent rails db:create db:migrate
+silent rails server --daemon -b 0.0.0.0
 echo "Rails server running at http://localhost:3000"
 echo "Good job everybody!"
 
+rm -f $logfile
