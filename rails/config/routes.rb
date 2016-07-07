e40bb8a6d556140195a092cebc4be381898d1624
Macaully James Muir <dev@macaullyjamesmuir.com>
2016-07-07 13:07:43 +0200

    Remove Session controller

--- rails/config/routes.rb
+++ rails/config/routes.rb
@@ -3,6 +3,5 @@ Rails.application.routes.draw do
   get 'auth/logout'
   get 'welcome/index'
   get 'dashboard/index'
-  resource :session, only: [:new, :create, :destroy]
   root 'welcome#index'
 end
--- rails/test/test_helper.rb
+++ rails/test/test_helper.rb
@@ -5,23 +5,4 @@ require 'rails/test_help'
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
-
-  # Sign in as the given user. Passing nil will result in a failed login
-  # attempt.
-  def sign_in_as(user)
-    post session_path, params: { session: { token: user&.token } }
-  end
-
-  # Sign in with some user
-  def sign_in
-    sign_in_as User.all.sample
-  end
-
-  def sign_out
-    delete session_path
-  end
-
-  def signed_in?
-    session.key?(:user_id)
-  end
 end
