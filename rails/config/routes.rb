a538d50c9990a2584655eeb5bc437212f306be49
Macaully James Muir <dev@macaullyjamesmuir.com>
2016-07-08 11:44:53 +0200

    Simplify login/logout route names

--- rails/config/routes.rb
+++ rails/config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  get 'auth/login'
-  get 'auth/logout'
+  get 'auth/login',  as: 'login'
+  get 'auth/logout', as: 'logout'
   get 'auth/callback'
+
   get 'dashboard', to: 'dashboard#index'
   root 'welcome#index'
 end
