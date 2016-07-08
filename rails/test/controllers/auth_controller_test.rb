b96aac4c436a972e3dff91b2a5ed45c63a65a432
Macaully James Muir <dev@macaullyjamesmuir.com>
2016-07-08 21:14:31 +0200

    Add tests for the login process
    Testing is hard 🤔

--- rails/test/controllers/auth_controller_test.rb
+++ rails/test/controllers/auth_controller_test.rb
@@ -1,4 +1,33 @@
 require 'test_helper'
 
 class AuthControllerTest < ActionDispatch::IntegrationTest
+
+  setup do
+    @internal_url = /^#{root_url}/
+    @external_url = /^(?!#{root_url}).*/
+  end
+
+  test "should update the session variable" do
+    user = User.all.sample
+    sign_in as: user
+    assert_equal session[:user_id], user.id
+    sign_out
+    assert_nil session[:user_id]
+  end
+
+  test "should redirect to external service if not logged in" do
+    get login_url
+    assert_redirected_to @external_url
+  end
+
+  test "should redirect to an internal page if already logged in" do
+    sign_in
+    get login_url
+    assert_redirected_to @internal_url
+
+    # Make sure we're not in a redirect loop
+    follow_redirect!
+    assert_response :success
+  end
+
 end
--- rails/test/test_helper.rb
+++ rails/test/test_helper.rb
@@ -1,8 +1,19 @@
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
+require 'mocha/mini_test'
 
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
+
+  def sign_in(as: User.all.sample)
+    AuthController.any_instance.stubs(:token_from).returns(as.token)
+    get auth_callback_url, params: { code: "test" }
+  end
+
+  def sign_out
+    get logout_url
+  end
+
 end
