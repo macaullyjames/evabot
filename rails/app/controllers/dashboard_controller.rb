6a30de1262b692acb6c85b4c513776573aea4303
Macaully James Muir <dev@macaullyjamesmuir.com>
2016-07-14 01:46:08 +0200

    Improve repo sorting in dashboard
    Repos are now sorted by owner, collaborator and organisation.

--- rails/app/controllers/dashboard_controller.rb
+++ rails/app/controllers/dashboard_controller.rb
@@ -1,7 +1,17 @@
 class DashboardController < ApplicationController
   def index
-    unless current_user
-      redirect_to root_url
+    return redirect_to root_url unless current_user
+    if current_user.remote
+      remote = current_user.remote
+      @owner_repos = remote.repositories(nil, affiliation: "owner")
+      @collaborator_repos = remote.repositories(nil, affiliation: "collaborator")
+      @org_repos = {}
+      remote.repositories(nil, affiliation: "organization_member")
+        .each do |repo|
+          owner_name  = repo.owner.login
+          @org_repos[owner_name] ||= []
+          @org_repos[owner_name] << repo
+        end
     end
   end
 end
