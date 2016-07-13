3487dd07f2c140fb82da3ee14ab9a4659fd4b1ab
Macaully James Muir <dev@macaullyjamesmuir.com>
2016-07-13 23:54:31 +0200

    Make login mandatory for dashboard
    This makes it a bit easier to work with for the time being.

--- rails/app/controllers/dashboard_controller.rb
+++ rails/app/controllers/dashboard_controller.rb
@@ -1,4 +1,7 @@
 class DashboardController < ApplicationController
   def index
+    unless current_user
+      redirect_to root_url
+    end
   end
 end
--- rails/test/controllers/dashboard_controller_test.rb
+++ rails/test/controllers/dashboard_controller_test.rb
@@ -3,6 +3,10 @@ require 'test_helper'
 class DashboardControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
     get dashboard_url
+    assert_redirected_to root_url
+
+    sign_in
+    get dashboard_url
     assert_response :success
   end
 
