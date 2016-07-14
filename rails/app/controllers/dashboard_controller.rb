6ff21754843ac015cfea08d40a2d953ce2a356c2
Macaully James Muir <dev@macaullyjamesmuir.com>
2016-07-14 16:14:11 +0200

    Only display repos the user has admin access to

--- rails/app/controllers/dashboard_controller.rb
+++ rails/app/controllers/dashboard_controller.rb
@@ -2,16 +2,14 @@ class DashboardController < ApplicationController
   def index
     return redirect_to root_url unless current_user
     if current_user.remote
-      remote = current_user.remote
-      @owner_repos = remote.repositories(nil, affiliation: "owner")
-      @collaborator_repos = remote.repositories(nil, affiliation: "collaborator")
-      @org_repos = {}
-      remote.repositories(nil, affiliation: "organization_member")
-        .each do |repo|
-          owner_name  = repo.owner.login
-          @org_repos[owner_name] ||= []
-          @org_repos[owner_name] << repo
-        end
+      user_repos = current_user.remote.repositories nil,
+        affiliation: "owner",
+        sort: "updated"
+      member_repos = current_user.remote.repositories nil,
+        affiliation: "organization_member",
+        sort: "updated"
+      member_repos.select! { |r| r.permissions.admin }
+      @remote_repos = user_repos + member_repos
     end
   end
 end
