deed762ebeaa19fab0a72ade78a74630c0254c83
Macaully James Muir <dev@macaullyjamesmuir.com>
2016-07-18 10:36:48 +0200

    Make tests pass without a valid client id

--- rails/test/controllers/auth_controller_test.rb
+++ rails/test/controllers/auth_controller_test.rb
@@ -11,10 +11,13 @@ class AuthControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "/login" do
+    stubbed_url = "https://github.com/login/oauth/authorize"
+    Octokit::Client.any_instance.stubs(:authorize_url).returns(stubbed_url)
+
     external_url = /^(?!#{root_url}).*/
 
     get login_url
-    assert_redirected_to external_url
+    assert_redirected_to stubbed_url
 
     sign_in
     get login_url
