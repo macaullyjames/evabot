bd1388cd88c75758dcef7cb579aeac4e11ddc704
Macaully James Muir <dev@macaullyjamesmuir.com>
2016-07-18 11:36:16 +0200

    Minor housecleaning

--- rails/app/models/user.rb
+++ rails/app/models/user.rb
@@ -8,24 +8,15 @@ class User < ApplicationRecord
   end
 
   def self.from_token token
-    # Try and create the user
-    user = User.find_by(token: token)
-    user ||= begin
-      remote = Octokit::Client.new(access_token: token)
+    User.find_by(token: token) || begin
+      remote = Octokit::Client.new access_token: token
       user = User.create token: token, username: remote.user.login
-    rescue Octokit::Unauthorized
-      return nil
-    end
-
-    # Add their repos
-    if user.repos.empty?
-      user.remote.repositories.each do |repo|
+      remote.repositories.each do |repo|
         if repo.permissions.admin
           Repo.create user_id: user.id, name: repo.name
         end
       end
+    rescue Octokit::Unauthorized
     end
-
-    return user
   end
 end
