44a998477930766200e8aae80b77a796b5c0f149
Macaully James Muir <dev@macaullyjamesmuir.com>
2016-07-22 20:50:48 +0200

    Only give thanks on new PRs

--- rails/app/controllers/events_controller.rb
+++ rails/app/controllers/events_controller.rb
@@ -3,16 +3,19 @@ class EventsController < ApplicationController
   skip_before_action :ensure_signed_in
 
   def pull_request_event
-    pr = params[:pull_request]
-    repo = Repo.find_by(
-      name: pr[:base][:repo][:name],
-      owner: pr[:base][:repo][:owner][:login]
-    )
-    eva.remote.add_comment(
-      repo.full_name,
-      pr[:number],
-      "Thanks for the PR 👌🏻"
-    )
+    action = request.request_parameters["action"]
+    if action == "opened"
+      pr = params[:pull_request]
+      repo = Repo.find_by(
+        name: pr[:base][:repo][:name],
+        owner: pr[:base][:repo][:owner][:login]
+      )
+      eva.remote.add_comment(
+        repo.full_name,
+        pr[:number],
+        "Thanks for the PR 👌🏻"
+      )
+    end
   end
 
 end
