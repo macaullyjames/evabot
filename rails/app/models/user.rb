29ad55abfcc690bf1dfccd86356e6321a08df75d
Macaully James Muir <dev@macaullyjamesmuir.com>
2016-07-28 15:02:56 +0200

    Create Syncable concern

--- rails/app/models/user.rb
+++ rails/app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  include Syncable
+
   has_and_belongs_to_many :organizations
   has_and_belongs_to_many :teams
   has_one :owner, as: :ownerable
@@ -15,51 +17,6 @@ class User < ApplicationRecord
 
   def repos
     owner.repos
-  end
-
-  def sync(by:)
-    if by == :fetching
-      remote_orgs = remote.orgs
-      remote_orgs.each do |o|
-        org = Organization.where(login: o.login).first_or_create
-        org.users << self
-        Owner.where(ownerable: org).first_or_create
-      end
-      orgs.each do |org|
-        org.destroy unless remote_orgs.any? do |o|
-          o.login == org.login
-        end
-      end
-
-      remote_repos = remote.repos nil, affiliation: :owner
-      repos.each do |repo|
-        repo.destroy unless remote_repos.any? do |r|
-          r.full_name == repo.full_name
-        end
-      end
-      remote_repos.each do |r|
-        repo = Repo.find_by owner: owner, name: r.name 
-        repo ||= Repo.create(
-          owner: owner,
-          name: r.name,
-          tracked: false
-        )
-      end
-    end
-
-    remote.user_teams.each do |t|
-      team = Team.find_by(remote_id: t.id)
-      team ||= Team.create(
-        organization: Organization.find_by(login: t.organization.login),
-        remote_id: t.id,
-        slug: t.slug,
-        permission: t.permission
-      )
-      team.users << self
-    end
-
-    repos.each { |r| r.sync by: :fetching, as: self }
-    teams.each { |t| t.sync by: :fetching, as: self }
   end
 
 end
